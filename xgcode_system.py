02ca07c28673604a43c4034cad6be20e0e00b801
shide530 <147078876+shide530@users.noreply.github.com>
2026-08-05 16:54:02 +0300

    Update xgcode_system.py

--- xgcode_system.py
+++ xgcode_system.py
@@ -14,7 +14,7 @@ def load_model():
 
 model = load_model()
 
-st.set_page_config(page_title="XGBoost Churn Prediction System"
+st.set_page_config(page_title="XGBoost Churn Prediction System",
                     layout="wide")
 
 # Collect user inputs
