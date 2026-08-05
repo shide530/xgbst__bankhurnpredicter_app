e8f17f65dbf5ac001762d4088893b9085f7c5783
shide530 <147078876+shide530@users.noreply.github.com>
2026-08-05 18:53:01 +0300

    Update xgcode_system.py

--- xgcode_system.py
+++ xgcode_system.py
@@ -15,12 +15,34 @@ def load_model():
 model = load_model()
 # Collect user inputs
 st.sidebar.header("Input Features")
-feature_1 = st.sidebar.number_input("Feature 1", value=0.0)
-feature_2 = st.sidebar.number_input("Feature 2", value=0.0)
-
+Age = st.sidebar.number_input("Age",min_value=18,max_value=84,value=20)
+CreditScore = st.sidebar.number_input("CreditScore",min_value=365,max_value=850 value=430)
+Tenure=st.sidebar.number_input("Tenure",min_value=0.00,max_value=10.00,value=0.00) 
+Balance=st.sidebar.number_input("Balance",min_value=0.00,max_value=221532.80,value=10000.00)
+NumOfProducts=st.sidebar.number_input("NumOfProducts",min_value=1.00,max_value=4.00,value=1.00)
+HasCrCard=st.sidebar.number_input("Has a CreditCard",[1,0])
+EstimatedSalary=st.sidebar.number_input("EstimatedSalary",min_value=106.670,max_value=199992.48,value=250000.00)
+complain=st.sidebar.number_input("complain",[1,0])
+Satisfaction_Score=st.sidebar.number_input("satisfaction rate",min_value=1.00,max_value=5.00,value=1.00)
+Point_Earned=st.sidebar.number_input("Points Earned",min_value=219.00,max_value=1000.00,value=250.00)
 # Build a single-row DataFrame matching your model's expected column names
-input_data = pd.DataFrame([[feature_1, feature_2]], columns=["Feature_1", "Feature_2"])
+
+input_data = pd.DataFrame([[Age,CreditScore,Tenure,Balance,NumOfProducts,HasCrCard,EstimatedSalary,complain,Satisfaction_Score,Points_Earned]],
+                          columns=["Age"
+                                  "CreditScore",
+                                   "Tenure",
+                                    "Balance",
+                                    "NumOfProducts",
+                                    "HasCrCard",
+                                     "IsActiveMember",
+                                     "EstimatedSalary,"
+                                      "Complain",
+                                      "Satisfaction Score",
+                                      "Point Earned"]
+                         )
 
 if st.button("Predict"):
     prediction = model.predict(input_data)[0]
-    st.success(f"Prediction: {prediction}")
+    result=("Customer is likely to churn if prediction ==1
+            else"Customer is likely to stay")
+    st.success(f"Prediction: {result}")
