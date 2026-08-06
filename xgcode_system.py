0ef776eee5dcd50f5e8c2c076db705422f0aecab
shide530 <147078876+shide530@users.noreply.github.com>
2026-08-06 15:27:21 +0300

    Update xgcode_system.py

--- xgcode_system.py
+++ xgcode_system.py
@@ -15,22 +15,16 @@ def load_model():
 model = load_model()
 # Collect user inputs
 st.sidebar.header("Input Features")
-# Collect user inputs
-st.sidebar.header("Input Features")
 
-# 1. First, define the widget to get user input
+RowNumber=st.sidebar.number_input("RowNumber",min_value=6,max_value=10000,value=6)
+CreditScore = st.sidebar.number_input("CreditScore",min_value=365,max_value=850,value=430)
 Geography_input = st.sidebar.selectbox("Geography", ["France", "Germany", "Spain"])
 Geo_map = {"France": 0, "Germany": 1, "Spain": 2}
 Geography_encoded = Geo_map[Geography_input]
 Gender_input = st.sidebar.selectbox("Gender", ["Female", "Male"])
 Gender_map = {"Female": 0, "Male": 1}
 Gender_encoded = Gender_map[Gender_input]
-CardType_input=st.sidebar.selectbox("Card Type",["GOLD","PLATINUM","DIAMOND","SILVER"])
-CardType_map={"GOLD": 0,"PLATINUM": 1,"DIAMOND": 2,"SILVER": 3}
-CardType_encoded=CardType_map[CardType_input]
-RowNumber=st.sidebar.number_input("RowNumber",min_value=6,max_value=10000,value=6)
 Age = st.sidebar.number_input("Age",min_value=18,max_value=84,value=20)
-CreditScore = st.sidebar.number_input("CreditScore",min_value=365,max_value=850,value=430)
 Tenure=st.sidebar.number_input("Tenure",min_value=0.00,max_value=10.00,value=0.00) 
 Balance=st.sidebar.number_input("Balance",min_value=0.00,max_value=221532.80,value=10000.00)
 NumOfProducts=st.sidebar.number_input("NumOfProducts",min_value=1.00,max_value=4.00,value=1.00)
@@ -39,16 +33,18 @@ IsActiveMember=st.sidebar.number_input("IsActiveMember",min_value=0,max_value=1,
 EstimatedSalary=st.sidebar.number_input("EstimatedSalary",min_value=106.670,max_value=199992.48,value=25000.00)
 Complain=st.sidebar.number_input("Complain",min_value=0,max_value=1,value=0)
 Satisfaction_Score=st.sidebar.number_input("satisfaction rate",min_value=1.00,max_value=5.00,value=1.00)
+CardType_input=st.sidebar.selectbox("Card Type",["GOLD","PLATINUM","DIAMOND","SILVER"])
+CardType_map={"GOLD": 0,"PLATINUM": 1,"DIAMOND": 2,"SILVER": 3}
+CardType_encoded=CardType_map[CardType_input]
 Point_Earned=st.sidebar.number_input("Points Earned",min_value=219.00,max_value=1000.00,value=250.00)
 # Build a single-row DataFrame matching your model's expected column names
 
-input_data = pd.DataFrame([[Geography_encoded,Gender_encoded,CardType_encoded,RowNumber,Age,CreditScore,Tenure,Balance,NumOfProducts,HasCrCard,IsActiveMember,EstimatedSalary,Complain,Satisfaction_Score,Point_Earned]],
-                          columns=["Geography",
-                                   "Gender",
-                                   "Card Type",
-                                   "RowNumber",
-                                   "Age",
+input_data = pd.DataFrame([[RowNumber,CreditScore,Geography_encoded,Gender_encoded,Age,Tenure,Balance,NumOfProducts,HasCrCard,IsActiveMember,EstimatedSalary,Complain,Satisfaction_Score,CardType_encoded,Point_Earned]],
+                          columns=["RowNumber",
                                   "CreditScore",
+                                   "Geography",
+                                   "Gender",
+                                   "Age",
                                    "Tenure",
                                     "Balance",
                                     "NumOfProducts",
@@ -57,6 +53,7 @@ input_data = pd.DataFrame([[Geography_encoded,Gender_encoded,CardType_encoded,Ro
                                      "EstimatedSalary",
                                       "Complain",
                                       "Satisfaction Score",
+                                      "Card Type",
                                       "Point Earned"]
                          )
 
